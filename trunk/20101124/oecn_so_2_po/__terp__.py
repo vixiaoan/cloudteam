655065863401ad3da7b42fc0aaa04ae681710f3f
wjfonhand@gmail.com <wjfonhand@gmail.com@9a2a1b34-6cbf-9973-a935-e8f7afe2ea40>
2010-11-25 06:33:28 +0000

    销售订单转采购订单代码

---
+++ trunk/20101124/oecn_so_2_po/__terp__.py
@@ -0,0 +1,21 @@
+﻿# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Created on 2010-11-24
+#    @author: stbrine@yahoo.com.cn
+#
+##############################################################################
+{
+    "name": "sale order to purchase ordre",
+    "author": "stbrine@yahoo.com.cn",
+    "version": "0.1",
+    "depends": ["sale","purchase"],
+    "description": """Recognized on the sale of certain business partners in order to automatically generate purchase orders""",
+    "init_xml": [],
+    "update_xml": [
+		'partner_view.xml',
+    ],
+    "demo_xml": [],
+    "installable": True,
+}
+
---
+++ trunk/20101124/oecn_so_2_po/partner.py
@@ -0,0 +1,19 @@
+﻿# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Created on 2010-11-24
+#    @author: stbrine@yahoo.com.cn
+#
+##############################################################################
+
+from osv import fields, osv
+from tools.translate import _
+
+class res_partner(osv.osv):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+    _description = 'Partner'
+    _columns = {
+        'createpurchase': fields.boolean('Auto Purchase Order', help="Create purchase of sale order Confirmation"),
+    }    
+res_partner()
