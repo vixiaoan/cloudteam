9492e0cc43f302dd8d93c678ef268c677c9a9179
sagetsang@gmail.com <sagetsang@gmail.com@9a2a1b34-6cbf-9973-a935-e8f7afe2ea40>
2011-04-20 02:53:38 +0000

    Format product_img_extra.py

--- trunk/20110415/oecn_retail_eshop/product_img_extra/product_img_extra.py
+++ trunk/20110415/oecn_retail_eshop/product_img_extra/product_img_extra.py
@@ -1,12 +1,11 @@
 from osv import osv, fields
 import urllib2
-import netsvc
 import base64
 class product_product(osv.osv):
     _name = 'product.product'
     _inherit = 'product.product'
     _description = 'Product'
-	
+
     def _get_image(self, cursor, user, ids, name, arg, context=None):
         image = {}
         opener = urllib2.build_opener()
@@ -16,10 +15,10 @@ class product_product(osv.osv):
         for id in ids:
             image[id] = pic
         return image
-	
+
     _columns = {
         'image_link' : fields.text('ImageLink'), 
         'image' : fields.function(_get_image, method=True, string='Product Image', type='binary', store=True), 
     }
- 
+
 product_product()
