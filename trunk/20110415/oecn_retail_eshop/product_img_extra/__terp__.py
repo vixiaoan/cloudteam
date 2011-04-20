20f468662b852fc756cb1fb981aed86a71b699e3
sagetsang@gmail.com <sagetsang@gmail.com@9a2a1b34-6cbf-9973-a935-e8f7afe2ea40>
2011-04-20 02:48:10 +0000

    产品首图能通过制定的图片链接予以显示：（估算 0.5 day） 基础功能已经完成,可以使用Link接来显示图片. GTK与Web均可显示图片,但布局仍需作一定调整.

---
+++ trunk/20110415/oecn_retail_eshop/product_img_extra/__terp__.py
@@ -0,0 +1,21 @@
+#-*- encoding: utf-8 -*-
+##############################################################################
+
+{
+    "name" : "product_image_extra",
+     
+    "version" : "1.0",
+     
+    "depends" : ["product"],
+     
+    "init_xml" : [],
+     
+    "update_xml" : ["product_img_extra_view.xml"],
+     
+    "installable" : True, 
+     
+    "active" : False, 
+     
+    "author" : "Sage",
+    
+}
