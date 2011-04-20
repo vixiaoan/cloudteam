7f775aa9acc84b14eb3c352c389be07849d76b6d
sagetsang <sagetsang@gmail.com>
2011-04-20 02:48:10 +0000

    产品首图能通过制定的图片链接予以显示：（估算 0.5 day） 基础功能已经完成,可以使用Link接来显示图片. GTK与Web均可显示图片,但布局仍需作一定调整.

---
+++ 20110415/oecn_retail_eshop/product_img_extra/product_img_extra.py
@@ -0,0 +1,25 @@
+from osv import osv, fields
+import urllib2
+import netsvc
+import base64
+class product_product(osv.osv):
+    _name = 'product.product'
+    _inherit = 'product.product'
+    _description = 'Product'
+	
+    def _get_image(self, cursor, user, ids, name, arg, context=None):
+        image = {}
+        opener = urllib2.build_opener()
+        res = self.read(cursor, user, ids, ['image_link'])
+        imageLink = res[0]['image_link']
+        pic = base64.encodestring(opener.open(imageLink).read())
+        for id in ids:
+            image[id] = pic
+        return image
+	
+    _columns = {
+        'image_link' : fields.text('ImageLink'), 
+        'image' : fields.function(_get_image, method=True, string='Product Image', type='binary', store=True), 
+    }
+ 
+product_product()
