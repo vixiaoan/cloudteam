403d9bd16aa46e4dfee1426d5cf7b5153d4423c0
wjfonhand@gmail.com <wjfonhand@gmail.com@9a2a1b34-6cbf-9973-a935-e8f7afe2ea40>
2011-04-20 15:10:56 +0000

    L21提交的按用户查看产品模块

---
+++ trunk/20110415/oecn_retail_eshop/productbysaleman/__terp__.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+{
+	"name" : "other inf",
+	"version" : "1.0",
+	"author" : "andy lin",
+	"website" : "andy3027@gmail.com",
+	"category" : "Generic Modules/Others",
+	"depends" : ["base","product","hr","process"],
+	"description" : """product
+        """,
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : ["productbysaleman.xml","productbysaleman_wizard.xml",
+                        "productbysaleman_data.xml",
+                                "security/product_security.xml","security/ir.model.access.csv"
+           ],
+	"active": False,
+	"installable": True
+}
