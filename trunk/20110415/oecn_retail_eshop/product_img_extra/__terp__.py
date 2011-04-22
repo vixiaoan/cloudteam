07904b5b43ac7ba4ba6cf0076de006911abdc695
wjfonhand@gmail.com <wjfonhand@gmail.com@9a2a1b34-6cbf-9973-a935-e8f7afe2ea40>
2011-04-22 06:13:45 +0000

    可能由于编码问题此模块在linux下导入不识别，已解决

--- trunk/20110415/oecn_retail_eshop/product_img_extra/__terp__.py
+++ trunk/20110415/oecn_retail_eshop/product_img_extra/__terp__.py
@@ -1,21 +1,33 @@
-#-*- encoding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+#    Created on 2011-04-21
+#    author:Joshua  
 ##############################################################################
 
 {
-    "name" : "product_image_extra",
-     
-    "version" : "1.0",
-     
-    "depends" : ["product"],
-     
+    "name" : "Show product image",
+    "version": "0.1",
+    "author" : "Sage@OECN",
     "init_xml" : [],
-     
-    "update_xml" : ["product_img_extra_view.xml"],
-     
-    "installable" : True, 
-     
-    "active" : False, 
-     
-    "author" : "Sage",
-    
+    "demo_xml" : [],
+    "update_xml": ['product_img_extra_view.xml'],
+    "active": False,
+    "installable": True,
+	"depends": ["base","product"]
 }
