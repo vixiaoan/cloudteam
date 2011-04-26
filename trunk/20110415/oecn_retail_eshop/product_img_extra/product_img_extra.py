eab4c6e5e7945d7fab07a43c6619de5279664f5b
sagetsang@gmail.com <sagetsang@gmail.com@9a2a1b34-6cbf-9973-a935-e8f7afe2ea40>
2011-04-26 08:23:07 +0000

    1. 改变product_img_extra.py继承关系,由继承product.product改变product.template 2. 优化product_img_extra的布局 3. ImageLink改用Char类型

--- trunk/20110415/oecn_retail_eshop/product_img_extra/product_img_extra.py
+++ trunk/20110415/oecn_retail_eshop/product_img_extra/product_img_extra.py
@@ -1,25 +1,25 @@
 from osv import osv, fields
 import urllib2
 import base64
-class product_product(osv.osv):
-    _name = 'product.product'
-    _inherit = 'product.product'
-    _description = 'Product'
+class product_template_img(osv.osv):
+    _name = 'product.template'
+    _inherit = 'product.template'
+    _description = 'Product Extra Image'
 
     def _get_image(self, cursor, user, ids, name, arg, context=None):
         image = {}
         opener = urllib2.build_opener()
         res = self.read(cursor, user, ids, ['image_link'])
-        imageLink = res[0]['image_link']
-        if imageLink:
-            pic = base64.encodestring(opener.open(imageLink).read())
+        image_link = res[0]['image_link']
+        if image_link:
+            pic = base64.encodestring(opener.open(image_link).read())
             for id in ids:
                 image[id] = pic
         return image
 
     _columns = {
-        'image_link' : fields.text('ImageLink'), 
-        'image' : fields.function(_get_image, method=True, string='Product Image', type='binary', store=True), 
+        'image_link' : fields.char('ImageLink', size=180),
+        'image' : fields.function(_get_image, method=True, string='Product Image', type='binary', store=False), 
     }
 
-product_product()
+product_template_img()
