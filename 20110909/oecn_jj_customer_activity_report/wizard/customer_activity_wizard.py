48dedc0c39c4bbece8f78cf79db17bebe8772f04
popkar77 <popkar77@9a2a1b34-6cbf-9973-a935-e8f7afe2ea40>
2011-09-22 09:17:38 +0000

    添加日期作为筛选条件

--- 20110909/oecn_jj_customer_activity_report/wizard/customer_activity_wizard.py
+++ 20110909/oecn_jj_customer_activity_report/wizard/customer_activity_wizard.py
@@ -27,23 +27,33 @@ class customer_activity_chart_wiazrd(osv.osv_memory):
         condition =[]
         ca_obj = self.pool.get('customer.activity')
         partner_obj = self.pool.get('res.partner')
+        address_obj = self.pool.get('res.partner.address')
+        
         res = self.read(cr, uid, ids, ['date_start', 'date_end','state_id','city','partner_status'], context=context)
         res = res and res[0] or {}
-        if res.get('state_id',False):
-            condition.append(('state_id','=',res['state_id']))
         if res.get('city',False):
             condition.append(('city','=',res['city']))
         if res.get('partner_status',False):
             condition.append(('partner_status','=',res['partner_status']))
         partner_ids = partner_obj.search(cr, uid, condition)
+        if res.get('state_id',False):
+            address_ids = address_obj.search(cr, uid, [('state_id','=',res['state_id']),('partner_id','in',partner_ids)])
+            partner_ids = []
+            if len(address_ids)>0:
+                addresss = address_obj.browse(cr, uid,address_ids)
+                for address in addresss:
+                    partner_ids.append(address.partner_id.id)
         if len(partner_ids)<=0:
-            raise ovs.eccept_osv(('Error!'),('The partners did not meet the conditions.'))
-        cr.execute('''SELECT res_partner.name,sum(moneydiff)+sum(pointdiff) 
-                        FROM customer_activity 
-                        LEFT JOIN res_partner on (customer_activity.partner_id = res_partner.id)
-                        WHERE partner_id in (%s)
-                        GROUP BY res_partner.name'''%(",".join(map(str,partner_ids))))
+            raise osv.except_osv(('Error!'),('The partners did not meet the conditions.'))
+        cr.execute('''SELECT res_partner.name,sum(moneydiff)+sum(pointdiff) \
+                        FROM customer_activity \
+                        LEFT JOIN res_partner on (customer_activity.partner_id = res_partner.id)\
+                        WHERE partner_id in (%s) \
+                        AND customer_activity.date<='%s' AND customer_activity.date>='%s'
+                        GROUP BY res_partner.name'''%(",".join(map(str,partner_ids)),res['date_end'],res['date_start']))
         result = cr.fetchall()
+        if len(result) <= 0:
+            raise osv.except_osv(('Error!'),('No Data!'))
         datas['form'] = result
         if res.get('id',False):
             datas['ids']=[res['id']]
