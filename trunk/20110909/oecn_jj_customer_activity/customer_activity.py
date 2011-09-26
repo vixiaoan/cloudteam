b2f4fc528f3e255c799b49c24949dfda17901f3c
popkar77 <popkar77@9a2a1b34-6cbf-9973-a935-e8f7afe2ea40>
2011-09-26 16:37:09 +0000

    1.根据r113修正。 2.修正自动导入的计划任务处于激活状态。

--- trunk/20110909/oecn_jj_customer_activity/customer_activity.py
+++ trunk/20110909/oecn_jj_customer_activity/customer_activity.py
@@ -28,16 +28,57 @@ class customer_activity(osv.osv):
         'moneydiff':fields.float('Moneydiff'),
     }
 
-    def download_data(self, cr, uid, point=0, money=0, context=None):
-        logger = netsvc.Logger()
-        partner_obj = self.pool.get('res.partner')
+    def generate_mail(self, cr, uid, body_text='',mail_type='',context={} ):
+        '''Generate the mail and send'''
         eta_obj = self.pool.get('email_template.account')
         etm_obj = self.pool.get('email_template.mailbox')
         user_obj = self.pool.get('res.users')
 
-        db=MySQLdb.connect(host=HOST,db=DB,user=USER,passwd=PASSWD)
+        today = datetime.date.today()
+
+        eta_ids = eta_obj.search(cr, uid, [])
+        if len(eta_ids) <= 0:
+        #Get the email account            
+            logger.notifyChannel('addons.'+self._name,netsvc.LOG_ERROR,'There is no effective email account')
+            return False
+
+        user = user_obj.browse(cr, uid, uid)
+        if not user.address_id.email:
+        #Get the email address 
+            logger.notifyChannel('addons.'+self._name,netsvc.LOG_ERROR,'There is no email for this account:%s'%user.name)
+            return False
+
+        email={
+            'subject':'[' + str(today) + '][Customer Avticity]' + mail_type,
+            'email_to':user.address_id.email,
+            'body_text':body_text,
+            'account_id':eta_ids[0],
+        }
+            
+        etm_id = etm_obj.create(cr, uid, email)
+        etm_obj.send_this_mail(cr, uid, [etm_id])
+        return True
+
+    def download_data(self, cr, uid, point=0, money=0, context=None):
+        '''Get the data from the website'''
+        logger = netsvc.Logger()
+        partner_obj = self.pool.get('res.partner')
+        
+
+        body_error_text = False
+        body_text = False
+        mail_type = ''
         today = datetime.date.today()
         oneday = datetime.timedelta(days=1)
+
+        try:
+            db=MySQLdb.connect(host=HOST,db=DB,user=USER,passwd=PASSWD)
+        except Exception:
+            logger.notifyChannel('addons.'+self._name, netsvc.LOG_ERROR,'MySQL Connect Failed.')
+            body_error_text = '[Error]MySQL Connect Failed.' 
+            mail_type='[ERROR]'           
+            return self.generate_mail(cr, uid, body_error_text, mail_type, context)
+
         partner_ids = partner_obj.search(cr, uid, [])
         partners = partner_obj.browse(cr, uid, partner_ids)
         partner_name = []
@@ -52,6 +93,9 @@ class customer_activity(osv.osv):
         results = c.fetchall()
         email = {}
         body = []
+        if len(results) <= 0:
+            logger.notifyChannel('addons.'+self._name, netsvc.LOG_ERROR,'Error! There is no data get from website!')
+            body_error_text = 'Warning! There is no data get from website!'
         for result in results:            
             ids = partner_obj.search(cr, uid, [('name','=',result[0])])
             if len(ids) > 0:
@@ -71,25 +115,13 @@ class customer_activity(osv.osv):
                     body.append(str(result[0])+'|'+str(result[2])+'|'+str(result[1]))
                 
         if len(body)>0:
-            body_text='username|point|money \n'+'\n'.join(body)           
-            #generate mail
-            eta_ids = eta_obj.search(cr, uid, [])
-            if len(eta_ids) <= 0:            
-                logger.notifyChannel('addons.'+self._name,netsvc.LOG_ERROR,'There is no effective email account')
-                return False
-            user = user_obj.browse(cr, uid, uid)
-            if not user.address_id.email:
-                logger.notifyChannel('addons.'+self._name,netsvc.LOG_ERROR,'There is no email for this account:%s'%user.name)
-            email={
-                'subject':str(today)+'[Customer Avticity]',
-                'email_to':user.address_id.email,
-                'body_text':body_text,
-                'account_id':eta_ids[0],
-            }
-            
-            etm_id = etm_obj.create(cr, uid, email)
-            etm_obj.send_this_mail(cr, uid, [etm_id])
-
+            body_text='username|point|money \n'+'\n'.join(body)
+        elif body_error_text:
+            body_text = body_error_text
+            mail_type = '[ERROR]'
+        if body_text:       
+            #Generate mail
+            return self.generate_mail(cr, uid, body_text, mail_type, context)
         return True
 
 customer_activity()
--- trunk/20110909/oecn_jj_customer_activity/readme.txt
+++
@@ -1,7 +0,0 @@
-1.本模块需要MySQLdb
-2.有关数据库的配置在customer_activity.py里面配置
-例如：
-HOST = 'localhost'
-DB = 'jjdb'
-USER = 'root'
-PASSWD = '1'
