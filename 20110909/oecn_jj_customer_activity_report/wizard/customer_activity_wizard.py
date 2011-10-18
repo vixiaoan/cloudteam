db0fd43b31d0a7e76bf533e46766fefb428b2fd8
popkar77 <popkar77@9a2a1b34-6cbf-9973-a935-e8f7afe2ea40>
2011-10-18 01:29:21 +0000

    修改报表模板格式，添加包括零以及添加取由大到小前N挑记录

--- 20110909/oecn_jj_customer_activity_report/wizard/customer_activity_wizard.py
+++ 20110909/oecn_jj_customer_activity_report/wizard/customer_activity_wizard.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from osv import fields, osv
+LIMIT = 3
 
 class customer_activity_chart_wiazrd(osv.osv_memory):
     _name = 'customer_activity_chart_wizard'
@@ -10,13 +11,14 @@ class customer_activity_chart_wiazrd(osv.osv_memory):
         'state_id': fields.many2one("res.country.state", 'Fed. State'),
         'city': fields.char('City', size=128),
         'partner_status': fields.many2one('res.partner.status','Partner Status'),
+        'except_zero':fields.boolean('except zero?'),
     }
 
     
     def print_report(self, cr, uid, ids, context=None):
         """
         print the report
-        @param self: The object pointer.<F8>
+        @param self: The object pointer.
         @param cr: A database cursor
         @param uid: ID of the user currently logged in
         @param context: A standard dictionary
@@ -26,13 +28,14 @@ class customer_activity_chart_wiazrd(osv.osv_memory):
             context = {}
         datas = {}
         condition =[]
+        except_zero ='' 
         ca_obj = self.pool.get('customer.activity')
         partner_obj = self.pool.get('res.partner')
         address_obj = self.pool.get('res.partner.address')
         country_obj = self.pool.get('res.country.state')
         partner_status_obj = self.pool.get('res.partner.status')
         
-        res = self.read(cr, uid, ids, ['date_start', 'date_end','state_id','city','partner_status','partner_id'], context=context)
+        res = self.read(cr, uid, ids, ['date_start', 'date_end','state_id','city','partner_status','partner_id','except_zero'], context=context)
         res = res and res[0] or {}
         if context.get('active_model',False)=='res.partner' and context.get('active_ids',False):
             condition.append(('id','in',context['active_ids']))
@@ -54,18 +57,23 @@ class customer_activity_chart_wiazrd(osv.osv_memory):
                     partner_ids.append(address.partner_id.id)
         if len(partner_ids)<=0:
             raise osv.except_osv(('Error!'),('The partners did not meet the conditions.'))
+        if res.get('except_zero',False):
+            except_zero = ' AND (moneydiff > 0 or  pointdiff > 0) '
+            res['except_zero'] ='Except 0'
         cr.execute('''SELECT res_partner.name,sum(moneydiff)+sum(pointdiff) \
                         FROM customer_activity \
                         LEFT JOIN res_partner on (customer_activity.partner_id = res_partner.id)\
-                        WHERE customer_activity.partner_id in (%s) \
-                        AND customer_activity.date<='%s' AND customer_activity.date>='%s'
-                        GROUP BY res_partner.name'''%(",".join(map(str,partner_ids)),res['date_end'],res['date_start']))
+                        WHERE customer_activity.partner_id in (%s)  %s \
+                        AND customer_activity.date<='%s' AND customer_activity.date>='%s' \
+                        GROUP BY res_partner.name  \
+                        ORDER BY  sum(moneydiff)+sum(pointdiff) DESC \
+                        LIMIT %s'''%(",".join(map(str,partner_ids)), except_zero, res['date_end'],res['date_start'],LIMIT))
         result = cr.fetchall()
         if len(result) <= 0:
             raise osv.except_osv(('Error!'),('No Data!'))
         datas['form'] = result
         datas['condition'] = res
-        
+        print result
         if res.get('id',False):
             datas['ids']=[res['id']]
         datas['model'] = 'res.partner' 
