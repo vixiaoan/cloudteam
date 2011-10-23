b17cf6598f05de8d099c4e0cd5d09871b66a78b2
popkar77 <popkar77@9a2a1b34-6cbf-9973-a935-e8f7afe2ea40>
2011-10-23 14:12:07 +0000

    添加两个邮件列表参数

--- trunk/20110909/oecn_jj_customer_activity/test/mysqldb_test.py
+++ trunk/20110909/oecn_jj_customer_activity/test/mysqldb_test.py
@@ -14,7 +14,7 @@ PASSWD = '1'
 
 db=MySQLdb.connect(host=HOST,db=DB,user=USER,passwd=PASSWD)
 c=db.cursor()
-c.execute("show tables;")
+c.execute(("SELECT * FROM common_member where username='"+u'jerry\u7ea2\u67a3\u8010\u9ad8\u6e29'+"'").encode('utf-8'))
 res = c.fetchall()
 print res
 
